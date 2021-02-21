4f2480826caa1e90eaa437f66ebd9971da83a42d
MakeRafa <rafavazquez229@gmail.com>
2021-02-21 14:19:30 -0800

    creating stackviews for login page

--- Onboarding/Controller/HomeVC.swift
+++ Onboarding/Controller/HomeVC.swift
@@ -7,3 +7,21 @@
 
 import UIKit
 
+//class homeVC: UIViewController {
+//    
+//    let homeVC: UIButton = {
+//        let homeButton = UIButton()
+//        homeButton.setTitle("Login", for: .normal)
+//        homeButton.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+//        homeButton.layer.cornerRadius = 10
+//        homeButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.8470588235)
+//        homeButton.translatesAutoresizingMaskIntoConstraints = false
+//        return homeButton
+//    }()
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+//    }
+//}
+
--- Onboarding/Controller/LoginVC.swift
+++ Onboarding/Controller/LoginVC.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    let  password: UITextField = {
+        let password = UITextField()
+        password.translatesAutoresizingMaskIntoConstraints = false
+        password.autocorrectionType = .no
+        password.autocapitalizationType = .none
+        password.font = UIFont.systemFont(ofSize: 20)
+        password.text = nil
+        password.placeholder = "enter password"
+        return password
+    }()
+
     let loginButton: UIButton = {
         let loginButton = UIButton()
         loginButton.setTitle("Login", for: .normal)
@@ -22,11 +33,29 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         setLoginButton()
+        setPassword()
     }
     
     func setLoginButton() {
+//        loginButton.addTarget(self, action: #selector(self.setHomeVC), for: .touchUpInside)
         self.view.addSubview(loginButton)
+        NSLayoutConstraint.activate([
+            loginButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            loginButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    func setPassword() {
+        self.view.addSubview(password)
+        NSLayoutConstraint.activate([
+            password.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            password.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            password.heightAnchor.constraint(equalTo: loginButton.heightAnchor),
+            password.widthAnchor.constraint(equalTo: loginButton.widthAnchor)
+        ])
     }
 }
 
